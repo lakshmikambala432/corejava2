afeef037e1004be6726a5ccd9b1486f73412c928
HP <HP@192.168.1.3>
2023-09-25 09:14:22 +0530

    first commit

---
+++ project1/src/com/tnsif/project/Encapsulation.java
@@ -0,0 +1,16 @@
+package com.tnsif.project;
+// program to demonstrate encapsulation in java
+
+public class Encapsulation {
+	//datamembers
+	public int serilano;
+	public String name;
+	public int age;
+	
+	//memberfunction
+	public void show() {
+		System.out.println("serialon"+ serialno+"\t"+ "name"+ name+ "\t"+ "age"+age);
+
+	}
+
+}
